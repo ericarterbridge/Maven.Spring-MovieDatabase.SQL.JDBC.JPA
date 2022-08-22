36c471dc5435f39d9876d8d2a2b540bbceab2409
eric <ericarterbridge@gmail.com>
2022-08-22 11:16:09 -0400

    added script.sql file

---
+++ src/main/resources/script.sql
@@ -0,0 +1,23 @@
+INSERT INTO MOVIES (TITLE, RUNTIME, GENRE, IMDB_SCORE, RATING)
+VALUES('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG');
+
+INSERT INTO MOVIES (TITLE, RUNTIME, GENRE, IMDB_SCORE, RATING)
+VALUES('Lavalantula', 83, 'Horror', 4.7, 'TV-14');
+
+INSERT INTO MOVIES (TITLE, RUNTIME, GENRE, IMDB_SCORE, RATING)
+VALUES('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13');
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Waltz With Bashir', 90, 'Documentary', 8.0, 'R');
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Spaceballs', 96, 'Comedy', 7.1, 'PG');
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Monsters Inc.', 92, 'Animation', 8.1, 'G');
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Burlesque', 119, 'Drama', 6.4, 'PG-13');
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Beetlejuice', 92, 'Comedy', 7.5, 'PG');
